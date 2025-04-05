33ff3c6651a2712dfa9c6e7d03574d97144b4d3f
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-05 12:35:35 +0530

    home page views started using .env variables

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,9 @@
+import os
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    port = os.getenv('PORT', 9000)
+    print(port)
+
     return render(request,'frontend/home.html')
