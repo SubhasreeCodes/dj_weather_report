358e085f87cc5e48db031cb9869aaa74bb5b6d57
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-04-15 12:07:25 +0530

    Update Settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = 'django-insecure-)ha0kadn&7p=bardobae9_486aaqyqgl)ts^0v4=hk=mq^$c8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'dj-weather-report.onrender.com',
+]
 
 
 # Application definition
